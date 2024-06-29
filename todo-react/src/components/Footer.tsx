a4809560eea14ed3ea2676f520fc02a1202144d7
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2024-06-29 17:44:20 +0530

    merge pages of collaboration

--- todo-react/src/components/Footer.tsx
+++ todo-react/src/components/Footer.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react"
 
-export default function Footer() {
+const Footer = () => {
   return (
     <div>
-        {/* Footer */}
+      {/* Footer */}
 
-    <div className="flex flex-wrap justify-between fixed bottom-0 rounded-lg bg-slate-50 drop-shadow-xl h-16 w-full p-4">
-      <div className="font-extrabold text-2xl text-[#F87777]">To-do list</div>
-      <div>@Odoo hackathon 2024</div>
-      <div>social links</div>
-    </div>
+      <div className="flex flex-wrap justify-between bottom-0 rounded-lg bg-slate-50 drop-shadow-xl h-16 w-full p-4">
+        <div className="font-extrabold text-2xl text-[#F87777]">To-do list</div>
+        <div>@Odoo hackathon 2024</div>
+        <div>social links</div>
+      </div>
     </div>
   )
 }
+
+export default Footer
--- todo-react/src/components/Header.tsx
+++ todo-react/src/components/Header.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react"
 
-export default function Header() {
+const Header = () => {
   return (
-    <div className="mx-auto max-w-7x2 px-2 sm:px-6 lg:px-8 p-5 m-0 h-24 rounded-lg bg-slate-50 drop-shadow-xl flex flex-wrap  justify-between">
-      <div className="flex font-extrabold text-4xl text-[#F87777] align-middle">To-do list</div>
+    <div className="mx-auto w-full px-2 sm:px-6 lg:px-8 p-5 m-0 h-24 bg-slate-50 drop-shadow-xl flex flex-wrap  justify-between">
+      <div className="flex font-extrabold text-4xl text-[#F87777] items-center">
+        To-do list
+      </div>
       <div className="flex">user</div>
     </div>
   )
 }
+
+export default Header
