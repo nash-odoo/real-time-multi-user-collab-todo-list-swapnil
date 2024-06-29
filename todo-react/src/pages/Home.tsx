0a97f2e916640d9b668f31d7d5ef033ee6cc1300
Milan Sagthiya <milan.sagthiya880@gmail.com>
2024-06-29 13:57:10 +0530

    improved padding, relocated footer

--- todo-react/src/pages/Home.tsx
+++ todo-react/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import './Home.css'
 import Header from '@/components/Header'
+import Footer from "@/components/Footer"
 
 const Home = () => {
   return <div>
@@ -9,17 +10,17 @@ const Home = () => {
     <div>
       <div className="m-6 p-4 bg-[#FAF7F2] rounded-lg text-center ">
 
-        <div className="flex flex-wrap text-white">
-          <div className="bg-red-300 rounded-lg w-1/12 min-w-28 h-32 p-3 m-auto">
+        <div className="flex flex-wrap text-white p-3">
+          <div className="bg-red-300 rounded-lg drop-shadow-lg w-1/12 min-w-28 h-32  p-2 m-auto">
             <div className="font-bold">Completed task</div>
             <div className="font-extrabold text-3xl">69</div>
 
           </div>
-          <div className="bg-[#F2B258] rounded-lg w-1/12 min-w-28 h-32 p-3 m-auto">
+          <div className="bg-[#F2B258] rounded-lg drop-shadow-lg w-1/12 min-w-28 h-32 p-2 m-auto">
             <div className="font-bold">Pending task</div>
             <div className="font-extrabold text-3xl">69</div>
           </div>
-          <div className="bg-blue-400 rounded-lg w-9/12 h-32 m-auto p-3 flex justify-between">
+          <div className="bg-blue-400 rounded-lg drop-shadow-lg w-9/12 h-32 m-auto my-4 p-2 flex justify-between">
             <div>
               <div className="font-bold">Tasks created</div>
               <div className="font-extrabold text-6xl">69</div>
@@ -33,13 +34,7 @@ const Home = () => {
       </div>
     </div>
 
-    {/* Footer */}
-
-    <div className="flex flex-wrap justify-between fixed bottom-0 rounded-lg bg-slate-50 drop-shadow-xl h-16 w-full p-4">
-      <div className="font-extrabold text-2xl text-[#F87777]">To-do list</div>
-      <div>@Odoo hackathon 2024</div>
-      <div>social links</div>
-    </div>
+    <Footer></Footer>
 
   </div>
 }
