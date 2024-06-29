4e6b9ca0ea96a922c377d72c8758f3c8fc3385da
Milan Sagthiya <milan.sagthiya880@gmail.com>
2024-06-29 13:11:45 +0530

    completed 3 divs

--- todo-react/src/pages/Home.tsx
+++ todo-react/src/pages/Home.tsx
@@ -7,21 +7,27 @@ const Home = () => {
     <Header></Header>
 
     <div>
-      <div className="m-6 bg-[#FAF7F2] rounded-lg text-center ">
-        
-        <div className="flex flex-wrap">
-          <div className="bg-red-300 rounded-lg w-1/12 h-32 p-3 m-auto">
+      <div className="m-6 p-4 bg-[#FAF7F2] rounded-lg text-center ">
+
+        <div className="flex flex-wrap text-white">
+          <div className="bg-red-300 rounded-lg w-1/12 min-w-28 h-32 p-3 m-auto">
             <div className="font-bold">Completed task</div>
             <div className="font-extrabold text-3xl">69</div>
-            
+
           </div>
-          <div className="bg-[#F2B258] font-bold rounded-lg w-1/12 h-32 p-3 m-auto">
+          <div className="bg-[#F2B258] rounded-lg w-1/12 min-w-28 h-32 p-3 m-auto">
             <div className="font-bold">Pending task</div>
             <div className="font-extrabold text-3xl">69</div>
           </div>
-          <div className="bg-blue-400 rounded-lg w-9/12 h-32 m-auto">
-            <div >Tasks created</div>
-            <div>Active users</div>
+          <div className="bg-blue-400 rounded-lg w-9/12 h-32 m-auto p-3 flex justify-between">
+            <div>
+              <div className="font-bold">Tasks created</div>
+              <div className="font-extrabold text-6xl">69</div>
+            </div>
+            <div>
+            <div className="font-bold">Active users</div>
+            <div className="font-extrabold text-6xl">69</div>
+            </div>
           </div>
         </div>
       </div>
