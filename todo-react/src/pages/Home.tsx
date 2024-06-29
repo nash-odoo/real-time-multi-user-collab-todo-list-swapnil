ffaa66ef3f5aac11ee91127a8bcde906bd8ba483
Milan Sagthiya <milan.sagthiya880@gmail.com>
2024-06-29 17:09:47 +0530

    added few images

--- todo-react/src/pages/Home.tsx
+++ todo-react/src/pages/Home.tsx
@@ -11,23 +11,32 @@ const Home = () => {
       <div className="m-6 p-4 bg-[#FAF7F2] rounded-lg text-center ">
 
         <div className="flex flex-wrap text-white p-3">
-          <div className="bg-red-300 rounded-lg drop-shadow-lg w-1/12 min-w-28 h-32  p-2 m-auto">
+          <div className="bg-[#F87777] rounded-lg drop-shadow-lg w-1/12 min-w-28 h-32  p-4 m-auto">
             <div className="font-bold">Completed task</div>
             <div className="font-extrabold text-3xl">69</div>
 
           </div>
-          <div className="bg-[#F2B258] rounded-lg drop-shadow-lg w-1/12 min-w-28 h-32 p-2 m-auto">
+          <div className="bg-[#F2B258] rounded-lg drop-shadow-lg w-1/12 min-w-28 h-32 p-4 m-auto">
             <div className="font-bold">Pending task</div>
             <div className="font-extrabold text-3xl">69</div>
           </div>
-          <div className="bg-blue-400 rounded-lg drop-shadow-lg w-9/12 h-32 m-auto my-4 p-2 flex justify-between">
+          <div className="bg-[#C4A49F] rounded-lg drop-shadow-lg w-9/12 h-32 m-auto my-4 p-4 flex justify-between">
             <div>
               <div className="font-bold">Tasks created</div>
-              <div className="font-extrabold text-6xl">69</div>
+              <div className="font-extrabold text-6xl">
+                69
+              </div>
             </div>
             <div>
               <div className="font-bold">Active users</div>
-              <div className="font-extrabold text-6xl">69</div>
+              <div className="flex">
+                <div className="border-4 rounded-full"><img src="src\assets\pngegg.png" alt=""/></div>
+                <div className="border-4 rounded-full"><img src="src\assets\pngegg.png" alt=""/></div>
+                <div className="border-4 rounded-full"><img src="src\assets\pngegg.png" alt=""/></div>
+                <div className="border-4 rounded-full"><img src="src\assets\pngegg.png" alt=""/></div>
+                <div className="border-4 rounded-full"><img src="src\assets\pngegg.png" alt=""/></div>
+
+              </div>
             </div>
           </div>
         </div>
@@ -35,7 +44,7 @@ const Home = () => {
     </div>
 
     <Footer></Footer>
-    
+
   </div>
 }
 
