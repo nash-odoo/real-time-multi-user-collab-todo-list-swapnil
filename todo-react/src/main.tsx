e7be9bc19ddac499d5c5cb4082d14902f6acde09
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2024-06-29 18:41:17 +0530

    mistake

--- todo-react/src/main.tsx
+++ todo-react/src/main.tsx
@@ -4,11 +4,16 @@ import App from "./App.tsx"
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
 import "react-tooltip/dist/react-tooltip.css"
+import { QueryClientProvider, QueryClient } from "react-query"
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
