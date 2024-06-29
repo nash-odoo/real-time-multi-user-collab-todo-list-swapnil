82d3419751a204834b2f84c82229aa6911f3d84a
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2024-06-29 12:00:28 +0530

    auth layout: added login and signup with form validation

--- todo-react/src/App.tsx
+++ todo-react/src/App.tsx
@@ -1,12 +1,31 @@
 import { Route, Routes } from "react-router-dom"
 import "./App.css"
 import Home from "./pages/Home"
+import Signup from "./pages/Signup"
+import AuthLayout from "./layouts/AuthLayout"
+import Login from "./pages/Login"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route
+          path="/signup"
+          element={
+            <AuthLayout>
+              <Signup />
+            </AuthLayout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <AuthLayout>
+              <Login />
+            </AuthLayout>
+          }
+        />
       </Routes>
     </>
   )
--- todo-react/src/layouts/AuthLayout.tsx
+++ todo-react/src/layouts/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react"
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
-    <main className="h-screen flex flex-col items-center justify-center">
+    <main className="h-screen flex flex-col items-center justify-center px-3 bg-[#C4A49F]">
       {children}
     </main>
   )
